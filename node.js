564ec97a2ea34bb18ac9c16e2e5b3afef7da9e03
alexdilion <alexdilion@protonmail.com>
2023-12-28 23:25:40 +0000

    Create node class

---
+++ node.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+export default class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
