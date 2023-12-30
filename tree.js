778365bb860499663392b95852fd931d7354b3cd
alexdilion <alexdilion@protonmail.com>
2023-12-30 22:05:51 +0000

    Add insert function

--- tree.js
+++ tree.js
@@ -16,13 +16,26 @@ export default class Tree {
 
         const data = this.#sortAndDeduplicate(array);
         const mid = Math.floor((data.length - 1) / 2);
-        console.log(data);
 
         const root = new Node(data[mid]);
         root.left = this.buildTree(data.slice(0, mid));
         root.right = this.buildTree(data.slice(mid + 1));
 
         return root;
+    }
+
+    insert(value, node = this.root) {
+        if (!node) return new Node(value);
+
+        if (value === node.value) {
+            return node;
+        } else if (value < node.value) {
+            node.left = this.insert(value, node.left);
+        } else if (value > node.value) {
+            node.right = this.insert(value, node.right);
+        }
+
+        return node;
     }
 
     prettyPrint(node, prefix = "", isLeft = true) {
